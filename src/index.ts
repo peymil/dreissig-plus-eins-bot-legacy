71c487cba153d2746bc47038139af4196fcc45cc
Emre Badem <emrebadem_@hotmail.com>
2022-01-14 16:38:14 +0300

    Added sqlite and typeORM.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import Discord from "discord.js";
 import commands from "./commands";
 import dotenv from "dotenv";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV == "development") {
   dotenv.config();
 }
+export type Context = {
+
+}
 const main = async () => {
+  const connection = await createConnection();
   const client = new Discord.Client();
   await client.login(process.env.BOT_TOKEN);
-  console.log("login succeed");
-  
-  const handleParseCommands = (msg: Discord.Message) => {
+
+  const handleParseCommands = (msg: Discord.Message,context?:Context) => {
     const commandArray = Object.values(commands);
     if (msg.author.bot) return;
     for (const command of commandArray) command(msg);
   };
   
-  client.on("message", handleParseCommands);
+  client.on("message", (msg) => handleParseCommands(msg));
 }
 main()
