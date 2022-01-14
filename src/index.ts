84a7c3a58a7923a7bc4fa8e4192ab158b8023a48
Emre Badem <emrebadem_@hotmail.com>
2022-01-15 02:11:55 +0300

    Changed ts configs.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-import Discord from "discord.js";
+import Discord, { Intents } from "discord.js";
 import commands from "./commands";
-import dotenv from "dotenv";
 import "reflect-metadata";
-import { createConnection } from "typeorm";
-
+import { createConnection, getRepository } from "typeorm";
 if (!process.env.NODE_ENV || process.env.NODE_ENV == "development") {
-  dotenv.config();
+  require("dotenv").config();
 }
-export type Context = {
-
-}
+export type Context = {};
 const main = async () => {
-  const connection = await createConnection();
-  const client = new Discord.Client();
+  const client = new Discord.Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  });
   await client.login(process.env.BOT_TOKEN);
 
-  const handleParseCommands = (msg: Discord.Message,context?:Context) => {
+  await createConnection();
+
+  console.log("initialization completed");
+
+  const handleParseCommands = (msg: Discord.Message, context?: Context) => {
     const commandArray = Object.values(commands);
     if (msg.author.bot) return;
     for (const command of commandArray) command(msg);
   };
-  
+
   client.on("message", (msg) => handleParseCommands(msg));
-}
-main()
+};
+main();
