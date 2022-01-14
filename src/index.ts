3df64cfd3cf58d83fda661441c35b0784b95671e
Emre Badem <emrebadem_@hotmail.com>
2022-01-14 05:15:17 +0300

    Added main function.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,17 @@ import dotenv from "dotenv";
 if (!process.env.NODE_ENV || process.env.NODE_ENV == "development") {
   dotenv.config();
 }
-const client = new Discord.Client();
-client.login(process.env.BOT_TOKEN);
-
-const handleParseCommands = (msg: Discord.Message) => {
-  const commandArray = Object.values(commands);
-  if (msg.author.bot) return;
-  for (const command of commandArray) command(msg);
-};
-
-client.on("message", handleParseCommands);
+const main = async () => {
+  const client = new Discord.Client();
+  await client.login(process.env.BOT_TOKEN);
+  console.log("login succeed");
+  
+  const handleParseCommands = (msg: Discord.Message) => {
+    const commandArray = Object.values(commands);
+    if (msg.author.bot) return;
+    for (const command of commandArray) command(msg);
+  };
+  
+  client.on("message", handleParseCommands);
+}
+main()
