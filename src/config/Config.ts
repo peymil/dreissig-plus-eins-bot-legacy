2cdb361e32b114c45436a19102f6956ca4a87d0f
Emre Badem <emrebadem_@hotmail.com>
2022-01-26 02:23:25 +0300

    Fixed a bug about validator assignation.

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,13 +1,14 @@
 import fs from "fs/promises";
 import ConfigValidators from "./ConfigValidators";
 class Config {
-  TOKEN = process.env.TOKEN || "";
+  BOT_TOKEN = process.env.BOT_TOKEN || "";
   CU_EVENT_TIMEOUT_MINUTES = 5;
   CU_EVENT_CHANCE_PERCANTAGE = 0.01;
   CAMI_MI_EVENT_TIMEOUT = 5;
 
   async loadValues(validators?: ConfigValidators) {
     const file = await fs.readFile(__dirname + "/../../data/config.json");
+    if (!file) return;
     const configFileJson = JSON.parse(file.toString());
 
     if (validators) {
@@ -16,7 +17,7 @@ class Config {
         const validator = validators[validatorKey];
         if (validator) {
           //@ts-ignore
-          objToWrite[validator] = validator(configFileJson[validator]);
+          validator(configFileJson[validator]);
         }
       }
     }
