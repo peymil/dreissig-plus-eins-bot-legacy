5d26192bb528572d3c1ae240c4eddbe40157ba08
Emre Badem <emrebadem_@hotmail.com>
2022-01-29 00:22:18 +0300

    Fixed a out of range bug in randon laugh generator.

--- src/utils/generateRandomLaugh.ts
+++ src/utils/generateRandomLaugh.ts
@@ -3,7 +3,7 @@ const alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZabcçdefgğhii̇jklmnoöprs
 const generateRandomLaugh = (min: number, max: number) => {
   const chars = [];
   for (let n = 0; n < Math.floor(Math.random() * max + 1) + min; n++) {
-    const random = Math.floor(Math.random() * 60);
+    const random = Math.floor(Math.random() * alphabet.length + 1);
     chars.push(random);
   }
   return chars.map((x) => alphabet[x]).join("");
