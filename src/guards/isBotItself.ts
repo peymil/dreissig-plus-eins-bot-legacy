caf05e6ebbb76eeba8029a02a41803fda9a2da20
Emre Badem <emrebadem_@hotmail.com>
2022-01-29 17:45:28 +0300

    Handled unmessage events.

--- src/guards/isBotItself.ts
+++ src/guards/isBotItself.ts
@@ -1,3 +1,4 @@
+import { Message } from "discord.js";
 import { ArgsOf, GuardFunction } from "discordx";
 
 const isBotItself: GuardFunction<ArgsOf<"messageCreate">> = async (
@@ -5,6 +6,7 @@ const isBotItself: GuardFunction<ArgsOf<"messageCreate">> = async (
   client,
   next
 ) => {
-  if (client.user?.id !== message.author.id) await next();
+  if (!(message instanceof Message) || client.user?.id !== message.author.id)
+    await next();
 };
 export default isBotItself;
