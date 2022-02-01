8c7caab34b91428c4bc42774a3ae958ebc1bccfb
Emre Badem <emrebadem_@hotmail.com>
2022-02-01 17:54:53 +0300

    Fixed a bug in cu. Renamed method

--- src/msgListeners/BanConditions.ts
+++ src/msgListeners/BanConditions.ts
@@ -2,15 +2,17 @@ import { DiscordAPIError } from "discord.js";
 import { ArgsOf, Client, Discord, Guard, On } from "discordx";
 import { BANNED_ACTIVITIES } from "../constants";
 @Discord()
-class Jokes {
+abstract class Jokes {
   @On("presenceUpdate")
-  async cu(
+  async forbiddenGames(
     [oldPresence, newPresence]: ArgsOf<"presenceUpdate">,
     client: Client
   ) {
     try {
       const activityName = newPresence.activities[0].name;
-      if (BANNED_ACTIVITIES.includes(activityName)) {
+      const prevActivityName = newPresence.activities[0].name;
+      const doesAlreadyPlaying = activityName === prevActivityName;
+      if (BANNED_ACTIVITIES.includes(activityName) && !doesAlreadyPlaying) {
         await newPresence.guild?.systemChannel?.send(
           `<@${newPresence.userId}> ${activityName} OYUNCUSU TESPIT EDILDI! ${activityName} OYUNCUSU TESPIT EDILDI! AMINA KOYAYIM ${activityName} OYUNCUSU`
         );
