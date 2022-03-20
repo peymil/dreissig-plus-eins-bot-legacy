b59d891ae5900fa3d883329557074d9c76294cfc
Emre Badem <emrebadem_@hotmail.com>
2022-03-20 04:31:07 +0300

    Seperate client logic.

---
+++ src/client.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+import { Client } from "discordx";
+import { createConnection } from "../node_modules/typeorm/index.js";
+import config from "./config";
+import { importx } from "@discordx/importer";
+import path from "path";
+import isBotItself from "./guards/isBotItself";
+import { intents } from "./constants";
+
+//Driver
+export const createClient = async () => {
+  const client = new Client({
+    intents,
+    guards: [isBotItself],
+  });
+  await config.loadValues().catch(() => config.saveValues());
+
+  client.on("ready", async () => {
+    console.log(">> Bot started");
+
+    await client.initApplicationCommands();
+    await client.initApplicationPermissions();
+  });
+
+  await createConnection();
+
+  await importx(path.join(__dirname, "/msgListeners/*.{ts,js}"));
+
+  await client.login(config.BOT_TOKEN); // provide your bot token
+  return client;
+};
--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,11 @@ import { importx } from "@discordx/importer";
 import path from "path";
 import isBotItself from "./guards/isBotItself";
 import { intents } from "./constants";
+import { createClient } from "./client.js";
 
 //Driver
 const main = async () => {
-  const client = new Client({
-    intents,
-    guards: [isBotItself],
-  });
-  await config.loadValues().catch(() => config.saveValues());
-
-  client.on("ready", async () => {
-    console.log(">> Bot started");
-
-    await client.initApplicationCommands();
-    await client.initApplicationPermissions();
-  });
-
   await createConnection();
-
-  await importx(path.join(__dirname, "/msgListeners/*.{ts,js}"));
-
-  await client.login(config.BOT_TOKEN); // provide your bot token
+  await createClient();
 };
 main();
