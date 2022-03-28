5a06a910ab5f4dc91e16b204126227360b185781
Emre Badem <emrebadem_@hotmail.com>
2022-03-28 19:03:42 +0300

    Add new word to laugh.

--- src/constants.ts
+++ src/constants.ts
@@ -1,27 +1,8 @@
 import { Intents } from "discord.js";
 
-export const BANNED_ACTIVITIES = [
-  "Genshin Impact",
-  "League Of Legends",
-  "Osu!",
-];
+export const BANNED_ACTIVITIES = ["Genshin Impact", "League Of Legends", "Osu!"];
 export const welcomeReplyWords = ["aleyküm selam", "as", "aleykümselam"];
-export const welcomeWords = [
-  "selamun aleyküm",
-  "selamunaleyküm",
-  "selamın aleyküm",
-  "selamınaleyküm",
-  "sa",
-];
-export const wordsToLaugh = [
-  "benis",
-  "penis",
-  "bok"
-]
-export const intents = [
-  Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MESSAGES,
-  Intents.FLAGS.GUILD_PRESENCES,
-];
-export const cumGibiLink =
-  "https://cdn.discordapp.com/attachments/468445665400193035/937757559903887500/kum.mp4";
+export const welcomeWords = ["selamun aleyküm", "selamunaleyküm", "selamın aleyküm", "selamınaleyküm", "sa"];
+export const wordsToLaugh = ["benis", "penis", "bok", "kaka"];
+export const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES];
+export const cumGibiLink = "https://cdn.discordapp.com/attachments/468445665400193035/937757559903887500/kum.mp4";
