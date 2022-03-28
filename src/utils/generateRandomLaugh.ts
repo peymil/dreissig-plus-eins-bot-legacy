731cc9902d38161015f93258a364e82f5f2adbc4
Emre Badem <emrebadem_@hotmail.com>
2022-03-29 00:37:30 +0300

    fix weird char output.

--- src/utils/generateRandomLaugh.ts
+++ src/utils/generateRandomLaugh.ts
@@ -1,14 +1,13 @@
-const alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZabcçdefgğhii̇jklmnoöprsştuüvyz";
-
-
-
+const alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZabcçdefgğhıijklmnoöprsştuüvyz";
 
 const generateRandomLaugh = (min: number, max: number) => {
   const chars = [];
   for (let n = 0; n < Math.floor(Math.random() * max + 1) + min; n++) {
-    const random = Math.floor(Math.random() * alphabet.length + 1);
-    chars.push(random);
+    const random = Math.floor(Math.random() * (alphabet.length - 1));
+
+    chars.push(alphabet[random]);
   }
-  return chars.map((x) => alphabet[x]).join("");
+  return chars.join("");
 };
+
 export default generateRandomLaugh;
